96a31779cba1b0f1c472d2e001438e4a3d3cb1a1
Criston Mascarenhas <criston2011@gmail.com>
2023-10-02 19:16:16 +0530

    final

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
 import * as path from "path"
-import typescript from "@rollup/plugin-typescript"
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
+import tsPaths from "vite-tsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [typescript(), react()],
+  plugins: [react(), tsPaths()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
@@ -15,12 +15,14 @@ export default defineConfig({
   },
   build: {
     outDir: "build",
-    sourcemap: false,
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
-    mockReset: true,
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom", "react-router-dom"],
+          "ant-vendor": ["antd", "moment"],
+        },
+      },
+    },
   },
 })
