31201411981d5e1cd32989ed7145a2af54dc6cbb
Criston Mascarenhas <criston2011@gmail.com>
2023-10-02 20:23:00 +0530

    final

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  server: {
+    host: "0.0.0.0",
+    port: 8080,
+  },
   build: {
     outDir: "build",
     sourcemap: true,
