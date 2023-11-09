3419d68e995831d6da114f12bc54f5474005bb60
Criston Mascarenhas <criston2011@gmail.com>
2023-11-10 00:59:39 +0530

    fix(app): :green_heart: fix build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+      "./runtimeConfig": "./runtimeConfig.browser",
     },
   },
   envPrefix: "REACT_",
